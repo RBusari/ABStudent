d13f60ab0a023ee61299fe65c47e8e6a45730ed1
RBusari <ruthbusari@gmail.com>
2015-12-13 16:08:27 +0100

    Create footerout.php

---
+++ includes/footerout.php
@@ -0,0 +1,12 @@
+<br><br>
+</div>
+
+<div class='afooter'><footer>
+        <div class='myscript'>© ABStudent</div>
+        <div class='terms'>About and Terms of use</div>
+        <div class='img'><img alt='Sign In' title='Sign In' src='img/facebook.png'/>
+            <img alt='Sign In' title='Sign In' src='img/google+.png'/></div>
+    </footer></div>
+</body>
+</html>
+<!--Footer outside the account-->
