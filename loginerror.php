9f2a0643cbf0b58d67ae57148ccead350ceed180
RBusari <ruthbusari@gmail.com>
2015-12-13 16:01:04 +0100

    Create loginerror.php

---
+++ loginerror.php
@@ -0,0 +1,20 @@
+<?php
+include '/includes/headerout.php'; ?>
+
+<div class='maincontent'>
+
+    <div class="errorpage">
+        <h1>Забравена парола или/и потребителско име?</h1>
+        <div class="errorcnt"><h2>Тогава...</h2>
+            <div><a href="/login.php">Опитайте отново</a>,<br> 
+                прочетете <a href="/instruct.php" target="_blank">инструкциите</a> към сайта <br>
+                или ни пишете на <a href="mailto: abs1@abstudent.com" target="_blank">abs1@abstudent.com</a>.<br><br>
+                Ще се постараем да Ви отговорим възможно най-скоро!</div></div>
+        <div class="imgsm"><img src="img/whitesm.png" alt="Smile" title="Smile Back :)"/></div>
+    </div>
+    
+    
+    
+</div>
+
+<?php include '/includes/footerout.php'; 
