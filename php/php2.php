5496611704f689dafa77d11f4b94b8725c9fa9b5
RBusari <ruthbusari@gmail.com>
2015-12-20 19:49:11 +0100

    Update php2.php

--- php/php2.php
+++ php/php2.php
@@ -6,10 +6,10 @@ $DBPass   = '-';
 $DBName   = '-';
 
 $con = new mysqli($DBServer, $DBUser, $DBPass, $DBName); 
- mysqli_query($con, 'SET NAMES utf8');
+ 
 // Check Connection
 if ($con->connect_error) {
   trigger_error('Database connection failed: '  . $con->connect_error, E_USER_ERROR);
 }
-
+mysqli_query($con, 'SET NAMES utf8');
 $admin['email']='alwaysbstudent@gmail.com';
