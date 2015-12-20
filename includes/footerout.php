4885e3a2ed6bf2b505aa62e0d31c2071c3eacaa2
esther-b <busari.esther@gmail.com>
2015-12-20 19:07:00 +0100

    Update footerout.php

--- includes/footerout.php
+++ includes/footerout.php
@@ -1,12 +1,17 @@
-<br><br>
-</div>
-
-<div class='afooter'><footer>
-        <div class='myscript'>© ABStudent</div>
-        <div class='terms'>About and Terms of use</div>
-        <div class='img'><img alt='Sign In' title='Sign In' src='img/facebook.png'/>
-            <img alt='Sign In' title='Sign In' src='img/google+.png'/></div>
-    </footer></div>
-</body>
-</html>
 <!--Footer outside the account-->
+<br><br>
+	</div>
+		<div class='afooter'>
+			<footer>
+			        <div class='myscript'>© Бъди Студент <?php echo date('Y'); ?></div>
+			        <div class='terms'><a href="instruct.php" alt="Условия за ползване">Инструкции</a> | <a href="about.php" alt="За нас">За нас</a></div>
+			        <div class='img'>
+			            <a href="https://www.facebook.com/be.student/" target="_blank" alt="Visit our Facebook page"><img alt='be.student on facebook' title='be.student on facebook' src='img/facebook.png'/></a>
+			            <a href="https://plus.google.com/u/3/105860019273948692996/posts" target="_blank" title="Visit our Google+Page"><img alt='BStudent on G+' title='Sign In' src='img/google+.png'/></a>
+			        </div>
+			</footer>
+		</div>
+	</body>
+</html>
+
+
