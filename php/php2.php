d47dd221eb983ea0f1c2ea254884da0beaa4f922
RBusari <ruthbusari@gmail.com>
2015-12-20 19:48:48 +0100

    Update php2.php

--- php/php2.php
+++ php/php2.php
@@ -6,7 +6,7 @@ $DBPass   = '-';
 $DBName   = '-';
 
 $con = new mysqli($DBServer, $DBUser, $DBPass, $DBName); 
- 
+ mysqli_query($con, 'SET NAMES utf8');
 // Check Connection
 if ($con->connect_error) {
   trigger_error('Database connection failed: '  . $con->connect_error, E_USER_ERROR);
